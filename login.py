da7affc5b991d6143eff9b877afc4a66ae5a058b
shraddha <shraddhad2@hexaware.com>
2022-09-22 13:31:09 +0530

    sql insert

---
+++ login.py
@@ -0,0 +1,15 @@
+usernames=[]
+passwords=[]
+name=[]
+def register(self):
+    self.names.append(input("Enter your name :"))
+    self.username.append(input("Username :"))
+    self.password.append(input("Password :"))
+
+def login(self):
+        self.username=input(("Enter username :"))
+        password=input("Enter password :")
+        if username in username
+        if the username of 
+
+    
